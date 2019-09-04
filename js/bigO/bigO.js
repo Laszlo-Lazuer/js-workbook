e542a0aed7d974c6198af7b69d0ec0ef37256a69
Laszlo Lazuer <visualjazz@gmail.com>
2019-09-04 18:36:09 -0500

    chore(quiz) - Adds linear time and calculation of bigo quiz1 and 2.

--- js/bigO/bigO.js
+++ js/bigO/bigO.js
@@ -1,6 +1,7 @@
 import {ex1} from './ex1.js';
 import {ex2} from './ex2.js';
 import {ex3} from './ex3.js';
+import {ex4} from './ex4_O1.js';
 
 // Big O Module
 
@@ -8,5 +9,6 @@ export const main = () => {
     document.getElementById("page-title").innerHTML = "Topic - Big O";
     // ex1();
     // ex2();
-    ex3();
+    // ex3();
+    ex4();
 };
---
+++ js/bigO/ex4_O1.js
@@ -0,0 +1,76 @@
+const boxes = [0,1,2,3,4,5];
+
+export const ex4 = () => {
+    document.getElementById('sub-title').innerText = "O(1) Constant Time - Same constant ammount of time, irrelevant of inputs, flat";
+    // compressFirstBox(large);
+    logFirstTwoBoxes(boxes); // O(2) - 2 operations every time.
+};
+
+
+function compressFirstBox(boxes) {
+    console.log(boxes[0]);
+}
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
+}
+
+
+// Quiz 1
+// https://repl.it/@aneagoie/BigO-Exercise-1
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function funChallenge(input) {
+    let a = 10;
+    a = 50 + 3;
+
+    for (let i = 0; i < input.length; i++) {
+        anotherFunction();
+        let stranger = true;
+        a++;
+    }
+    return a;
+}
+
+// Solution
+function funChallenge(input) { // O(n)
+    let a = 10; // O(1)
+    a = 50 + 3; // O(1)
+
+    for (let i = 0; i < input.length; i++) { // O(n)
+        anotherFunction(); // O(n) -> called depending on input size
+        let stranger = true; // O(n) -> called depending on input size
+        a++; // O(n) -> called depending on input size
+    }
+    return a; // O(1)
+}
+
+// 3 + n + n + n + n -> 3 + 4n
+// BIG O(3+4n)
+
+// Quiz 2
+// https://repl.it/@aneagoie/BigO-Exercise-2
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+    let a = 5; // O(1)
+    let b = 10; // O(1)
+    let c = 50; // O(1)
+
+    for (let i = 0; i < input; i++) { // O(n)
+        let x = i + 1; // O(n)
+        let y = i + 2; // O(n)
+        let z = i + 3; // O(n)
+
+    }
+
+    for (let j = 0; j < input; j++) { // O(n)
+        let p = j * 2; // O(n)
+        let q = j * 2; // O(n)
+    }
+
+    let whoAmI = "I don't know"; // O(1)
+}
+
+// Solution
+// 4 + n + n + n + n + n + n + n
+// 4 + 7n -> BIG O(4+7n)
